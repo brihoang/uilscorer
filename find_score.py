c6fc9d9b854a97910780d928d7afc9725f8a93bc
Brian <bri.hoang@gmail.com>
2014-03-09 13:58:06 -0500

    fixed some bugs in find_score, and enhanced display of scoreboard

--- find_score.py
+++ find_score.py
@@ -4,13 +4,20 @@ if os.path.exists("./scorekeeping/scoreboard.txt"):
 	scoreboard = open("./scorekeeping/scoreboard.txt")
 	problems = {}
 	for line in scoreboard:
-		problems[ line.split()[0] ] = 60
+		problems[ line.split()[0] ] = 0
+		if not "INCORRECT" in line:
+			problems[ line.split()[0] ] = 60
 	scoreboard = open("./scorekeeping/scoreboard.txt")
 	for line in scoreboard:
 		if "INCORRECT" in line:
 			problems[ line.split()[0] ] -= 5
 	s = 0
-	for key in problems.keys():
-		s += problems[key]
+	print "%-21s%-21s%-5s" % ("Problem Name", "Status", "Score")
+	for key in sorted(problems.keys()):
+		status = "CORRECT" if problems[key] > 0 else "INCORRECT" 
+		s = s+problems[key] if problems[key] > 0 else s
+		print "%-20s %-20s %-5d" % (key, status, problems[key] if status == "CORRECT" else 0)
 	
-	print s
+	print "%-42s%-d" % ("TOTAL",s)
+else:
+	print 0
