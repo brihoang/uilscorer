32745b8cd90ecfe3ba5aee7190cd4dea3c0bc15e
Brian <bri.hoang@gmail.com>
2014-03-09 04:02:17 -0500

    fixed another score keeping bug

--- find_score.py
+++ find_score.py
@@ -5,6 +5,7 @@ if os.path.exists("./scorekeeping/scoreboard.txt"):
 	problems = {}
 	for line in scoreboard:
 		problems[ line.split()[0] ] = 60
+	scoreboard = open("./scorekeeping/scoreboard.txt")
 	for line in scoreboard:
 		if "INCORRECT" in line:
 			problems[ line.split()[0] ] -= 5
